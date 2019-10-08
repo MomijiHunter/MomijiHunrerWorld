4c43b2192efae3ed6d2562fa0b338fa5f3be514a
kyon91 <56115683+kyon91@users.noreply.github.com>
2019-10-08 20:49:37 +0900

    Map Fixed

---
+++ MH/Assets/Player_kubota/FixMap.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixMap : MonoBehaviour
+{
+    Vector3 position;
+    void Start()
+    {
+        position = transform.position;
+    }
+    
+    void Update()
+    {
+        transform.position = position;
+    }
+}
