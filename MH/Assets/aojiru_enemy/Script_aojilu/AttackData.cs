28255e73fa2f82fe25491cd101ada82dd1a8a0c0
seiji <blueg0826@gmail.com>
2019-10-16 20:00:08 +0900

    吹っ飛びフラグの追加
    attackDataに吹っ飛びフラグを追加しました
    isBlowAway=trueのとき吹っ飛びます

--- MH/Assets/aojiru_enemy/Script_aojilu/AttackData.cs
+++ MH/Assets/aojiru_enemy/Script_aojilu/AttackData.cs
@@ -2,13 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-    public class AttackData : MonoBehaviour
-    {
-        [SerializeField] int attackPower;
-        public int AttackPower { get { return attackPower; } }
+public class AttackData : MonoBehaviour
+{
+    [SerializeField] int attackPower;
+    public int AttackPower { get { return attackPower; } }
 
-        public void SetAttackPower(int n)
-        {
-            attackPower = n;
-        }
+    /// <summary>
+    /// 吹っ飛ぶかどうか
+    /// </summary>
+    [SerializeField] bool isBlowAway=false;
+    public bool IsBlowAway { get { return isBlowAway; } }
+
+    public void SetAttackPower(int n)
+    {
+        attackPower = n;
     }
+
+    public void SetBlowAway(bool flag)
+    {
+        isBlowAway = flag;
+    }
+}
