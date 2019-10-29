44601efce1e519faa46ab8d3176e585558f7dda0
kyon91 <56115683+kyon91@users.noreply.github.com>
2019-10-29 16:22:13 +0900

    カメラ修正

--- MH/Assets/aojiru_enemy/Script_aojilu/LoadObject.cs
+++ MH/Assets/aojiru_enemy/Script_aojilu/LoadObject.cs
@@ -62,7 +62,7 @@ namespace aojilu {
         {
             playerCamera.GetCinemachineComponent<CinemachineTransposer>().m_XDamping = 0;
             playerCamera.GetCinemachineComponent<CinemachineTransposer>().m_YDamping = 0;
-            yield return new WaitForSeconds(0.2f);
+            yield return new WaitForSeconds(0.3f);
             playerCamera.GetCinemachineComponent<CinemachineTransposer>().m_XDamping = 1;
             playerCamera.GetCinemachineComponent<CinemachineTransposer>().m_YDamping = 3.3f;
         }
