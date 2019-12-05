6a2a0b9086fe98a5642a26c5c0b75c2277a62e37
DESKTOP-CMOH7M4\blueg <blueg0826@gmail.com>
2019-12-05 12:03:20 +0900

    タイトルロゴの修正
    ・タイトルロゴのアニメーションを変更しました
    ・初めてタイトルシーンに戻った時と、2回目以降でUIの初期位置が変わるようにしました（要相談）

---
+++ MH/Assets/aojiru/aojiru_shader/Script/LogoShaderCtrl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// GradientAlphaシェーダーの_posXを変更するスクリプト
+/// </summary>
+public class LogoShaderCtrl : MonoBehaviour
+{
+    [SerializeField] Image myImage;
+    [SerializeField,Range(0,1)] float posx;
+    float beforePosx;
+    Material targetMaterial;
+
+    private void Start()
+    {
+        targetMaterial = myImage.material;
+        beforePosx = SetMaterialPosx(posx);
+    }
+
+    private void Update()
+    {
+        if (posx != beforePosx)
+        {
+            beforePosx=SetMaterialPosx(posx);
+        }
+    }
+
+    /// <summary>
+    /// GradientAlphaシェーダーの_posXをfの値に変更する
+    /// _posxは透明化が始まる位置
+    /// </summary>
+    float SetMaterialPosx(float f)
+    {
+        targetMaterial.SetFloat("_PosX",f);
+        return f;
+    }
+}
