2a5fcc4490048f9db4da2044a9d926e17abc87dd
kyon91 <56115683+kyon91@users.noreply.github.com>
2019-12-19 00:41:14 +0900

    ポーズ・リタイア（タイトルに戻る）実装

---
+++ MH/Assets/Player_kubota/PauseMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] Image PauseMenuPanel;
+    [SerializeField] Selectable ContinueButton;
+
+    public void Continue()
+    {
+        Time.timeScale = 1;
+        PauseMenuPanel.gameObject.SetActive(false);
+    }
+
+    public void Retire()
+    {
+        SceneManager.LoadScene("Title");
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Time.timeScale == 0)
+            {
+                Time.timeScale = 1;
+                PauseMenuPanel.gameObject.SetActive(false);
+            }
+            else
+            {
+                Time.timeScale = 0;
+                PauseMenuPanel.gameObject.SetActive(true);
+                ContinueButton.Select();
+            }
+        }
+    }
+}
